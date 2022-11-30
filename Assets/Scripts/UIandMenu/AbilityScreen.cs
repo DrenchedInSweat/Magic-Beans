22b23a6e39ee35ce83114d54bd2a0913b33b662b
FlamingoeZpZ <52292616+FlamingoeZpZ@users.noreply.github.com>
2022-11-30 14:55:43 -0500

    Upgrades

--- Assets/Scripts/UIandMenu/AbilityScreen.cs
+++ Assets/Scripts/UIandMenu/AbilityScreen.cs
@@ -1,40 +1,97 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using Characters;
+using Characters.BaseStats;
+using Characters.Upgrades;
+using TMPro;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using Weapons;
 
-public class AbilityScreen : MonoBehaviour
+namespace UIandMenu
 {
-    [SerializeField] Button[] buttons;
-    
-    
-    
-    // Start is called before the first frame update
-    void OnEnable()
+    public class AbilityScreen : MonoBehaviour
     {
-        foreach (Button button in buttons)
+
+        [SerializeField] private GameObject selections;
+    
+        public void Init(Player component, UpgradeBaseSo[] generateUpgradeSet, int len)
         {
-            DisplayInfo(button);
-            button.onClick.AddListener(AbilitySelect);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            Time.timeScale = 0;
+            Transform parent = transform.GetChild(0);
+            transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Select an Upgrade";
+            for (int i = 0; i < len; i++)
+            {
+                Transform host = Instantiate(selections, parent).transform;
+                UpgradeBaseSo upgrade = generateUpgradeSet[i];
+                
+                host.GetChild(0).GetChild(0).GetComponent<Image>().sprite = upgrade.Sprite;
+                host.GetChild(1).GetComponent<TextMeshProUGUI>().text = upgrade.Name;
+                host.GetChild(2).GetComponent<TextMeshProUGUI>().text = upgrade.Description;
+                //Please god don't lose information doing this
+                host.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    if (upgrade is WeaponUpgradeSo s)
+                    {
+                        if (s.ApplyToAll)
+                        {
+                            component.UpgradeAttackCharacter(s, (int)s.MyApplicableWeapons); // Apply to affected weapon... 
+                        }
+                        else
+                        {
+                            SelectWeapon(component, s);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        component.UpgradeCharacter((CharacterUpgradeSo)upgrade);
+                    }
+
+                    ResetGame();
+                });
+            }
+        }
+
+        private void ResetGame()
+        {
+            Time.timeScale = 1;
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Destroy(gameObject);
+        }
+
+        private void SelectWeapon(Player p, WeaponUpgradeSo upgrade)
+        {
+            Transform parent = transform.GetChild(0);
+            transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = "Select a Weapon to Upgrade";
+            int num = (int)upgrade.MyApplicableWeapons;
+
+            for (int i = 0; i < parent.childCount; ++i)
+            {
+                Destroy(parent.GetChild(i).gameObject);
+            }
+
+
+            foreach (Weapon w in p.ShootingCapability.Weapons)
+            {
+                WeaponStatsSo s = w.GetStats<WeaponStatsSo>();
+                if(((int)s.WeaponType & num) == 0) continue;
+                Transform host = Instantiate(selections, parent).transform;
+                
+                host.GetComponent<Button>().onClick.AddListener(() =>
+                {
+                    w.Upgrade(upgrade);
+                    ResetGame();
+                });
+                
+                host.GetChild(0).GetChild(0).GetComponent<Image>().sprite = s.Sprite;
+                host.GetChild(1).GetComponent<TextMeshProUGUI>().text = s.Name;
+                host.GetChild(2).GetComponent<TextMeshProUGUI>().text = s.Description;
+            }
         }
     }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
-    void DisplayInfo(Button button)
-    {
-        
-    }
-
-    void AbilitySelect()
-    {
-
-    }
-
 }
+
